1b6a43b37f9c4faeb7791009da53f9ef61840daa
Kingcedru <byishimocedrick@gmail.com>
2024-03-05 11:59:44 +0200

    added post method

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+
+const users = [];
+
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    console.log(users);
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/register");
+  }
+});
+
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
