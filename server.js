8625f04fd2e44cba08ceaf7a53b08ec7c55bf164
Kingcedru <byishimocedrick@gmail.com>
2024-03-05 11:02:08 +0200

    project setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.listen(3000);
